ca3c180f2ea843713ff17fb9d14578bb20161f1d
RF <rumana2020uk@gmail.com>
2019-05-20 09:18:39 +0530

    initial commit

---
+++ NIBMConnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBMConnect
+//
+//  Created by R F on 5/20/19.
+//  Copyright © 2019 Rumana Fazani-COBSCComp171p-007. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
